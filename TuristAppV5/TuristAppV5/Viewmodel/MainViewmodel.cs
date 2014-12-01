b76818b7b62fb91b39ad24a7c51564eb25c39838
Daniel <danielwinther@hotmail.dk>
2014-12-01 15:47:57 +0100

    master

---
+++ TuristAppV5/TuristAppV5/Viewmodel/MainViewmodel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuristAppV5.Viewmodel
+{
+    class MainViewmodel
+    {
+    }
+}
